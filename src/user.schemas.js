bee760f0d4ebda25d6da9ec475370abe34449953
Benedict <benedictasare2@gmail.com>
2024-05-17 09:42:14 +0000

    min max length

--- src/user.schemas.js
+++ src/user.schemas.js
@@ -1,13 +1,27 @@
 import * as yup from 'yup'
 
+const MIN_LENGTH = {
+    name: 3,
+    email: 3,
+    city: 3,
+    country: 3
+}
+
+const MAX_LENGTH = {
+    name: 20,
+    email: 50,
+    city: 30,
+    country: 15
+}
+
 export const addUser = {
     schema: {
         body: {
             yupSchema: yup.object().shape({
-                name: yup.string(),
-                email: yup.string().email(),
-                city: yup.string(),
-                country: yup.string()
+                name: yup.string().min(MIN_LENGTH.name).max(MAX_LENGTH.name),
+                email: yup.string().email().min(MIN_LENGTH.email).max(MAX_LENGTH.email),
+                city: yup.string().min(MIN_LENGTH.city).max(MAX_LENGTH.city),
+                country: yup.string().min(MIN_LENGTH.country).max(MAX_LENGTH.country),
             })
         },
     },
@@ -15,12 +29,17 @@ export const addUser = {
 
 export const updateUser = {
     schema: {
+        params: {
+            yupSchema: yup.object().shape({
+                id: yup.number().required(),
+            })
+        },
         body: {
             yupSchema: yup.object().shape({
-                name: yup.string(),
-                email: yup.string().email(),
-                city: yup.string(),
-                country: yup.string()
+                name: yup.string().min(MIN_LENGTH.name).max(MAX_LENGTH.name),
+                email: yup.string().email().min(MIN_LENGTH.email).max(MAX_LENGTH.email),
+                city: yup.string().min(MIN_LENGTH.city).max(MAX_LENGTH.city),
+                country: yup.string().min(MIN_LENGTH.country).max(MAX_LENGTH.country),
             })
         },
     },
