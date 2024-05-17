780b1426fc804d070bb64618965f2c67b8307ee8
Benedict <benedictasare2@gmail.com>
2024-05-17 09:49:02 +0000

    helmet

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 
 import mainRoutes from './main.routes.js';
 import userRoutes from './user.routes.js';
@@ -7,6 +8,7 @@ const app = express();
 const port = 3000
 
 app.use(express.json());
+app.use(helmet());
 
 app.use('/', mainRoutes);
 app.use('/v1/users', userRoutes);
