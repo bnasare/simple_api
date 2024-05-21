9de4da8f9851a29c1e11e9a92efea56460adea58
Benedict <benedictasare2@gmail.com>
2024-05-21 23:56:27 +0000

    documentation

--- src/models/persistence/user.dao.js
+++ src/models/persistence/user.dao.js
@@ -1,14 +1,32 @@
 import users from '../data/users.data';
 
+/**
+ * Retrieves a user by their unique ID.
+ *
+ * @param {number} userID - The unique identifier of the user to be retrieved.
+ * @returns {object|undefined} - The user object if found, otherwise undefined.
+ */
 const get = (userID) => {
     const getAUser = users.find((user) => user.id === userID);
     return getAUser;
 }
 
+/**
+ * Retrieves all users.
+ *
+ * @returns {Array} - An array of all user objects.
+ */
 const getAll = () => {
     return users;
 }
 
+/**
+ * Updates the details of an existing user.
+ *
+ * @param {number} userID - The unique identifier of the user to be updated.
+ * @param {object} newDetails - An object containing the updated details of the user.
+ * @returns {object|boolean} - The updated user object if the user was found and updated, or false if the user was not found.
+ */
 const update = (userID, newDetails) => {
     let existingUser = null;
     let userIndex;
@@ -24,18 +42,30 @@ const update = (userID, newDetails) => {
         return false;
     }
 
-    const updatedUser = {...existingUser, ...newDetails};
+    const updatedUser = { ...existingUser, ...newDetails };
     users.splice(userIndex, 1, updatedUser);
 
     return updatedUser;
 };
 
+/**
+ * Inserts a new user with the provided details.
+ *
+ * @param {object} details - An object containing the details of the new user.
+ * @returns {object} - The newly created user object.
+ */
 const insert = (details) => {
     const newUser = { id: users.length + 1, ...details };
     users.push(newUser);
     return newUser;
 };
 
+/**
+ * Removes a user by their unique ID.
+ *
+ * @param {number} userID - The unique identifier of the user to be removed.
+ * @returns {boolean} - True if the user was found and removed, or false if the user was not found.
+ */
 const remove = (userID) => {
     const index = users.findIndex(user => user.id === userID);
 
@@ -46,4 +76,4 @@ const remove = (userID) => {
     return false;
 };
 
-export default {get, getAll, update, insert, remove};
+export default { get, getAll, update, insert, remove };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,23 +1,53 @@
 import userDao from '../models/persistence/user.dao';
 
+/**
+ * Retrieves a user by their unique ID.
+ *
+ * @param {number} userID - The unique identifier of the user to be retrieved.
+ * @returns {object|undefined} - The user object if found, otherwise undefined.
+ */
 const getUser = (userID) => {
     return userDao.get(userID);
 }
 
+/**
+ * Retrieves all users.
+ *
+ * @returns {Array} - An array of all user objects.
+ */
 const getAllUsers = () => {
     return userDao.getAll();
 }
 
+/**
+ * Updates the details of an existing user.
+ *
+ * @param {number} userID - The unique identifier of the user to be updated.
+ * @param {object} details - An object containing the updated details of the user.
+ * @returns {object|boolean} - The updated user object if the user was found and updated, or false if the user was not found.
+ */
 const updateUser = (userID, details) => {
     return userDao.update(userID, details);
 }
 
+/**
+ * Adds a new user with the provided details.
+ *
+ * @param {object} details - An object containing the details of the new user.
+ * @returns {object} - The newly created user object.
+ */
 const addUser = (details) => {
-    return  userDao.insert(details);
+    return userDao.insert(details);
 }
 
+/**
+ * Removes a user by their unique ID.
+ *
+ * @param {number} userID - The unique identifier of the user to be removed.
+ * @returns {boolean} - True if the user was found and removed, or false if the user was not found.
+ */
 const removeUser = (userID) => {
     return userDao.remove(userID);
 }
 
-export default {getUser, getAllUsers, updateUser, addUser, removeUser}
+export default { getUser, getAllUsers, updateUser, addUser, removeUser }
